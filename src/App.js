f8a7ef1c1c988595f72c4d2e18fc8f85557636c2
LaiBeloni <86491599+LaiBeloni@users.noreply.github.com>
2021-07-11 13:36:15 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Teste from './componente/Teste';
+
+
+function App() {
+    return ( <
+        Teste / >
+    );
+}
+
+export default App;
---
+++ src/componente/Teste.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Teste extends Component {
+    render() {
+        return (
+            <div>
+            <img src="bg.jpg" alt="imagem"/>
+            </div>
+        );
+    }
+}
+
+export default Teste;
