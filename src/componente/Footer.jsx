cb55bf7cda6109395558dd89da9d292c3a379d41
LaiBeloni <86491599+LaiBeloni@users.noreply.github.com>
2021-07-11 14:01:03 -0300

    Create Footer.jsx

---
+++ src/componente/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+              <a href="https://github.com/LaiBeloni/atividade">Ver no Git</a>
+            </div>
+        );
+    }
+}
+
+export default Footer;
